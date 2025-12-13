3059aab6db5636963ce1d9b429db5ee655f024a3
Bored008 <dahiyahimanshu80.com>
2025-12-13 19:38:53 +0530

    everything done

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,15 @@ import TaskInput from "./components/Tasks/TaskInput.jsx";
 
 export default function App() {
   return (
-    <div className="bg-slate-900">
-      <div className="m-9 flex flex-wrap h-full">
-        <div className="w-full">
-          <Header />
-        </div>
-        <div className="w-full">
-          <TaskInput />
-        </div>
-        <div className="bg-gray-700 rounded-2xl w-full">
-          <Footer />
-        </div>
+    <div className="m-9 flex flex-wrap">
+      <div className="w-full">
+        <Header />
+      </div>
+      <div className="w-full">
+        <TaskInput />
+      </div>
+      <div className="bg-gray-700 rounded-2xl w-full">
+        <Footer />
       </div>
     </div>
   );
